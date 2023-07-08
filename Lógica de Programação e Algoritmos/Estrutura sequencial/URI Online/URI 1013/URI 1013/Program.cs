f03d07a44930d16a9504bed7e8973c53ca29628c
joao <hypergamerfrr@gmail.com>
2023-07-08 16:02:46 -0300

    att

---
+++ Lógica de Programação e Algoritmos/Estrutura sequencial/URI Online/URI 1013/URI 1013/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace uri1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, maiorAB, maior;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            a = int.Parse(valores[0]);
+            b = int.Parse(valores[1]);
+            c = int.Parse(valores[2]);
+
+            maiorAB = (a + b + Math.Abs(a - b)) / 2;
+
+            maior = (maiorAB + c + Math.Abs(maiorAB - c)) / 2;
+
+            Console.WriteLine(maior + " eh o maior");
+
+        }
+    }
+}
